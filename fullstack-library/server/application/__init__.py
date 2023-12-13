a53d6fd8242d2664ef85303d92345916fa33c742
olliebalabil <oliverbalabil@gmail.com>
2023-12-13 15:08:08 +0000

    env

--- fullstack-library/server/application/__init__.py
+++ fullstack-library/server/application/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wumfxrod:1LRvnB-2v36E81EwsDLpXq6_YkYId_c5@flora.db.elephantsql.com/wumfxrod'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
 db = SQLAlchemy(app)
 
 from application import routes
